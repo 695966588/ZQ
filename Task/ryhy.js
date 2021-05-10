6487faa9863bcfde643f912d4a7e4059f2cc4329
695966588 <82691921+695966588@users.noreply.github.com>
2021-05-10 14:23:51 +0800

    Update ryhy.js

--- Task/ryhy.js
+++ Task/ryhy.js
@@ -14,7 +14,7 @@ var roomcount,unlockno,id
 let shouldplan0,shouldplant1,shouldplant2,shouldplan3,ahouldplant4
 let status;
 status = (status = ($.getval("ryhystatus") || "1") ) > 1 ? `${status}` : ""; // 账号扩展字符
-var ryhyheaderArr = ['{\"bs\":\" CDMA\",\"osVersion\":\" iOS 13.50\",\"Host\":\"bp-api.coohua.com\",\"pkgId\":\" 271\",\"Accept-Encoding\":\"gzip, deflate, br\",\"deviceId\":\" 0\",\"wechatId\":\" \",\"brand\":\" Apple\",\"channel\":\" AppStore\",\"Cache-Control\":\"no-cache\",\"appVersion\":\" 1.0.3\",\"accessKey\":\" 111e4128a74700976553a0b03188137a_212430736\",\"romVersion\":\" iOS 13.50\",\"Connection\":\"keep-alive\",\"User-Agent\":\"ryhy-mobile/1 CFNetwork/1126 Darwin/19.5.0\",\"os\":\" iOS\",\"Accept-Language\":\"zh-cn\",\"Content-Type\":\" application/json\",\"oaid\":\" \",\"Accept\":\"*/*\",\"blackBox\":\" \",\"Content-Length\":\"37\",\"gps\":\" default\"}']
+var ryhyheaderArr = ['{"bs":"CDMA","osVersion":"iOS 11.30","pkgId":"271","Host":"bp-api.coohua.com","Accept-Encoding":"br, gzip, deflate","deviceId":"79002F4C-D37E-48F5-AB96-91EC61DD7AB5","gps":"default","brand":"Apple","channel":"AppStore","Connection":"keep-alive","accessKey":"0cb67401f2374b2a1fa0b1a8677e721f_227683607","appVersion":"1.0.3","romVersion":"iOS 11.30","Accept-Language":"zh-cn","os":"iOS","Content-Type":"application/json","User-Agent":"ryhy-mobile/1 CFNetwork/897.15 Darwin/17.5.0","Accept":"*/*","oaid":"","blackBox":"","Content-Length":"37","wechatId":""}']
 var ryhyadheaderArr = []
 var ryhyadbodyArr = []
 let ryhyheader = $.getdata('ryhyheader')
